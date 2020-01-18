85e67a1a54d27b8f7867ce9d207da5a10ba5290e
Sanuja - XPS <sanujac@live.com>
2020-01-18 10:44:34 +0530

    Initial Commit

---
+++ sanuja.py
@@ -0,0 +1,8 @@
+from my_planning_graph import PlanningGraph
+from air_cargo_problems import air_cargo_p1
+
+ac_problem = air_cargo_p1()
+ac_pg_serial = PlanningGraph(ac_problem, ac_problem.initial).fill()
+# In(C1, P2) and In(C2, P1) have inconsistent support when they first appear in
+# the air cargo problem, 
+inconsistent_support_literals = [expr("In(C1, P2)"), expr("In(C2, P1)")]
