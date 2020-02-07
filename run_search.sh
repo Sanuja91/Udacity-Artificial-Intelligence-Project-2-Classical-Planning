7dba07a396026ca0f087a7f2bd00ac0a6305ba71
Sanuja - XPS <sanujac@live.com>
2020-02-07 19:11:55 +0530

    Finished project

--- run_search.sh
+++ run_search.sh
@@ -1,3 +1,4 @@
 # pypy3 run_search.py -p 1 -s 1 2 3 4 5 6 7 8 9 10 11 > run_search_results_acp_p1.txt
 python run_search.py -p 2 -s 1 2 3 4 5 6 7 8 9 10 11 > run_search_results_acp_p2.txt
-python run_search.py -p 3 -s 1 4 5 6 7 8 9 > run_search_results_acp_p3.txt
+# python run_search.py -p 3 -s 1 4 5 6 7 8 9 > run_search_results_acp_p3.txt
+# python run_search.py -p 4 -s 1 4 5 6 8 9 > run_search_results_acp_p4.txt
